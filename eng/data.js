d2c8e26f7f3cf1faafa9f07731e69f516355f762
MaratYskak <yskakmarat2@gmail.com>
2024-05-25 13:21:23 +0500

    upd eng

--- eng/data.js
+++ eng/data.js
@@ -9,5 +9,10 @@ export default [
     "id": 1,
     "russian": `Исторически многие отдельные науки, такие как физика и психология, входили в состав философии. Однако они считаются отдельными академическими дисциплинами в современном понимании этого слова.`
 },
+{
+  "arabic": `The juxtaposition of ontological and epistemological paradigms precipitates an intricate dialectic within the realm of metaphysical inquiry. Pervasive throughout philosophical discourse is the axiomatic presupposition of the dichotomy between the noumenal and the phenomenal, engendering a profound conundrum regarding the veracity of human cognition. The epistemic frameworks that delineate our understanding are invariably ensnared in a labyrinthine matrix of heuristic methodologies and axiological evaluations, rendering the pursuit of unequivocal truths a Sisyphean endeavor. This incessant quest for certitude epitomizes the quintessential nature of philosophical contemplation.`,
+  "id": 2,
+  "russian": `Сопоставление онтологических и эпистемологических парадигм порождает сложную диалектику в сфере метафизических исследований. В философском дискурсе широко распространена аксиоматическая предпосылка дихотомии между ноуменальным и феноменальным, порождающая глубокую загадку относительно правдивости человеческого познания. Эпистемические рамки, очерчивающие наше понимание, неизменно попадают в ловушку лабиринтной матрицы эвристических методологий и аксиологических оценок, что делает поиск однозначных истин сизифовым усилием. Этот непрекращающийся поиск уверенности воплощает квинтэссенцию философского созерцания.`
+},
 
 ]
