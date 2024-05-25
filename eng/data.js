e9cdf33a7569f9dacc9dc36f0979a7bacd08a792
MaratYskak <yskakmarat2@gmail.com>
2024-05-25 13:01:46 +0500

    test eng

---
+++ eng/data.js
@@ -0,0 +1,13 @@
+export default [
+  {
+      "arabic": `Philosophy is a systematic study of general and fundamental questions concerning topics like existence, reason, knowledge, value, mind, and language. It is a rational and critical inquiry that reflects on its own methods and assumptions.`,
+      "id": 0,
+      "russian": `Философия — это систематическое исследование общих и фундаментальных вопросов, касающихся таких тем, как существование, разум, знание, ценность, разум и язык. Это рациональное и критическое исследование, размышляющее над собственными методами и предположениями.`
+  },
+  {
+    "arabic": `Historically, many of the individual sciences, such as physics and psychology, formed part of philosophy. However, they are considered separate academic disciplines in the modern sense of the term.`,
+    "id": 1,
+    "russian": `Исторически многие отдельные науки, такие как физика и психология, входили в состав философии. Однако они считаются отдельными академическими дисциплинами в современном понимании этого слова.`
+},
+
+]
